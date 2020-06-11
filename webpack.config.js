b0636c5a69f48345533ea1e47f8c2f979c71ed60
Nicole <nicoledbanta@gmail.com>
2020-06-11 13:48:51 -0700

    Setup React dev environment

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const DIST_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
